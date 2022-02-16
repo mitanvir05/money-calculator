89b79b15692a86bedd38badf11e641c40e88587d
mitanvir05 <tanvir.cse05@gmail.com>
2022-02-17 02:30:17 +0600

    Without Code Clean

--- js/apps.js
+++ js/apps.js
@@ -8,17 +8,24 @@ function calculate() {
     //Expense Calculation
     let totalExpenses = Number(foodExpences.value) + Number(rentExpences.value) + Number(clothesExpences.value);
     totalExpensesAmount.innerText = totalExpenses;
-
     //Balance Calculation
     balanceAmount.innerText = incomeAmount.value - totalExpenses;
+}
 
+//Savings Calculation
+function save() {
+    let savePerchant = document.getElementById('save-perchant');
+    let incomeAmount = document.getElementById('income-amount');
+    let savingAmount = document.getElementById('saving-amount');
+    let balanceAmount = document.getElementById('balance-amount');
+    let remainingBalance = document.getElementById('remaining-balance');
+    //Saving Ammount Calculation
+    let savingAmountResult = ((incomeAmount.value) * (savePerchant.value)) / 100;
+    savingAmount.innerText = savingAmountResult;
+    //Remaining Balance Calculation
 
-
-
-
-
-
-
+    var remainingBalanceResult = Number(balanceAmount.innerText) - Number(savingAmount.innerText);
+    remainingBalance.innerText = remainingBalanceResult;
 
 
 }
